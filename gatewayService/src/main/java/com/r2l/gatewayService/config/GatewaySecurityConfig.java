521576641d08ddb040abfbab2dd20f056a3c1a63
Leonan Marques <leonan.marques886@gmail.com>
2025-07-05 12:28:35 -0300

    chore: migrate gateway service to WebFlux, update dependencies and security configuration

---
+++ gatewayService/src/main/java/com/r2l/gatewayService/config/GatewaySecurityConfig.java
@@ -0,0 +1,24 @@
+// src/main/java/com/r2l/gatewayService/config/GatewaySecurityConfig.java (ou similar)
+
+package com.r2l.gatewayService.config; // Ajuste o pacote conforme sua estrutura
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http
+				.csrf(ServerHttpSecurity.CsrfSpec::disable)
+				.authorizeExchange(exchange -> exchange
+						.anyExchange().permitAll()
+				);
+		return http.build();
+	}
+}
